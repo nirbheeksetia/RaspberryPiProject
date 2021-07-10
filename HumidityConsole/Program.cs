2b5f8de42668b3ae2ea4ccdeb5f3a05738e3093b
nirbheeksetia <singhnirbheek@gmail.com>
2021-07-10 11:13:55 +0200

    commit2

---
+++ HumidityConsole/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using SensorLibrary;
+using Microsoft.Azure.Devices.Client;
+using System.Text;
+
+namespace HumidityConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HumidityClass sensor = new HumidityClass(18, 2000);
+            while (true)
+            {
+                var humidityData = sensor.ReadData();
+                var connectionString = "[the connection string of your device]";
+
+                using var deviceClient = DeviceClient.CreateFromConnectionString(connectionString);
+
+                //// Set various callback messages: connection status, device twin, a single named method, multiple methods
+                //SetVariousCallbackMethods(deviceClient);
+
+                //// open connection explicitly
+                deviceClient.OpenAsync().Wait();
+
+                //// Force the retrieval of the devicetwin
+                //ForceDeviceTwinRetrieval(deviceClient);
+
+                //// send a single message
+                SendSingleMessage(deviceClient,humidityData);
+
+                //// send multiple message in batch in one call
+                //SendBatchOfMessages(deviceClient);
+
+                //// Send a file to the Blob storage configured in the IoT Hub
+                //SendFileToBlobStorage(deviceClient);
+
+                //// Stall application closing
+            }
+        }
+        private static void SendSingleMessage(DeviceClient deviceClient,int humidity)
+        {
+            string jsonData = "{ \"humVal\":"+humidity.ToString()+"}";
+
+            var message = new Message(Encoding.ASCII.GetBytes(jsonData));
+
+            message.Properties.Add("messagetype", "normal");
+
+            deviceClient.SendEventAsync(message).Wait();
+
+            Console.WriteLine("A single message is sent");
+        }
+    }
+}
--- SensorLibrary/HumidityClass.cs
+++ SensorLibrary/HumidityClass.cs
@@ -7,22 +7,39 @@ namespace SensorLibrary
 {
     public class HumidityClass
     {
-        private int ledPin = 4;
-        private int time = 600000;
+        private int ledPin;
+        private int time;
         public HumidityClass(int ledpin,int timeint) 
         {
             this.ledPin = ledpin;
             this.time = timeint;
         }
-        public void ReadData() {
+        public int ReadData() {
             GpioController test = new GpioController();
             using GpioController controller = new();
             controller.OpenPin(ledPin);
-            while (true)
+            var returnVal = -1;
+            //while (true)
             {
                 var val = controller.Read(ledPin);
-                Console.WriteLine(val.ToString());
+                //Console.WriteLine(val.ToString());
+                //Console.WriteLine(((int)val).ToString());
+                if (val == 0)
+                {
+                    Console.WriteLine("High");
+                    returnVal = 1;
+                }
+                else if (val == 1)
+                {
+                    Console.WriteLine("Low");
+                    returnVal = 0;
+                }
+                else
+                {
+                    Console.WriteLine("Unvalid");
+                }
                 Thread.Sleep(time);
+                return returnVal;
             }
         }
     }
